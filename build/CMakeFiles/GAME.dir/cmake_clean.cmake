f80654c1d4b9616775160a56086384e6cd130949
Javier Salazar <sal18764@uvg.edu.gt>
2023-08-01 18:41:25 -0600

    Makefile creation

---
+++ build/CMakeFiles/GAME.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GAME.dir/src/main.cpp.o"
+  "CMakeFiles/GAME.dir/src/main.cpp.o.d"
+  "GAME"
+  "GAME.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GAME.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "string"
+#include "iostream"
+
+int main(){
+    std::cout << "hello world" << std::endl;
+};
+
+/* 
+sera necesario implementar lo siguiente: 
+un sistema de colision 
+componentes
+update del framerate
+update de las fisicas
+movimiento de los elementos
+
+*/
+
